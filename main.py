cab6c6519cca73f17db852cb14c2850d382efa9f
Arunkj203 <arunkj203@gmail.com>
2025-11-01 21:22:48 +0530

    nnn

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from .solve import *
 from .model_config import get_model_and_tokenizer
 
 from datasets import Dataset
-
+from .config import *
 
 def main():
 
@@ -15,15 +15,26 @@ def main():
     dataset_name = "SVAMP"
 
     # Load model and tokenizer
-    model , tokenizer = get_model_and_tokenizer()
+    # model , tokenizer = get_model_and_tokenizer()
 
     print(f"Model and tokenizer loaded for {dataset_name}.")
+
+    # Test functions
+    load_memory()
+
+    print(f"\nTotal primitives in memory: {len(primitive_metadata)}")
+    for i, (pid, meta) in enumerate(primitive_metadata.items()):
+        print(f"{i+1:03d}. {pid}  →  {meta.get('name', '')}")
+        if i >= 30:  # show only the first 30 to avoid flooding
+            break
+
+
     # solve(dataset_name,"train","Training", model, tokenizer)
     # solve(dataset_name,,"Training", model, tokenizer)
     # generate_phase1_analysis(dataset_name, "train", model, tokenizer)
     # generate_phase2_execution("SVAMP_train_phase1_analysis.json", model, tokenizer)
 
-    generate_phase4_execution("SVAMP_train_phase2_execution.json", model, tokenizer)
+    # generate_phase4_execution("SVAMP_train_phase2_execution.json", model, tokenizer)
 
 if __name__ == "__main__":
     main()
