9336a7ef681b54aa840216476931d19fa44711f6
Arunkj203 <arunkj203@gmail.com>
2025-11-13 23:46:59 +0530

    gsm8k base l testing

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from . import config as mem
 
 
 
-
-
 def main():
 
     # dataset_name = sys.argv[1] # e.g., "SVAMP"
@@ -34,13 +32,13 @@ def main():
     # SVAMP:
     # generate_phase1_analysis(svamp,"svamp", model, tokenizer, output_dir="Base_L")
     # generate_phase2_execution("svamp_test_phase1_analysis.json", model, tokenizer, output_dir="Base_L")
-    generate_phase3_execution("svamp_test_phase2_execution.json", model, tokenizer, output_dir="Base_L")
+    # generate_phase3_execution("svamp_test_phase2_execution.json", model, tokenizer, output_dir="Base_L")
 
     # GSM8k:
     
-    # generate_phase1_analysis(gsm8k,"gsm8k", model, tokenizer, output_dir="Base_L")
-    # generate_phase2_execution("gsm8k_test_phase1_analysis.json", model, tokenizer, output_dir="Base_L")
-    # generate_phase3_execution("gsm8k_test_phase2_execution.json", model, tokenizer, output_dir="Base_L")
+    generate_phase1_analysis(gsm8k,"gsm8k", model, tokenizer, output_dir="Base_L")
+    generate_phase2_execution("gsm8k_test_phase1_analysis.json", model, tokenizer, output_dir="Base_L")
+    generate_phase3_execution("gsm8k_test_phase2_execution.json", model, tokenizer, output_dir="Base_L")
 
 
 
