2dac3dcf066453b8a47e64335f25730e6ebc5ed8
Arunkumar J <arunkj203@gmail.com>
2025-11-15 12:40:50 -0500

    Merge

--- phase_2/generate_primitive.py
+++ phase_2/generate_primitive.py
@@ -258,7 +258,7 @@ Rules:
     )
 
     modules = analysis.get("selected_modules", [])
-    modules_str = "; ".join([f"{m['name']}" for m in modules])
+    modules_str = "; ".join(modules)
 
     user_prompt = f"""
 Problem Type: {analysis.get('problem_type', 'Unknown')}
